79b226f0c840fe4766a4ddecf037493bd3d69250
KulcsRX21 <60647365+KulcsRX21@users.noreply.github.com>
2023-06-11 08:55:36 +0200

    service done

--- src/csaladfa.service.ts
+++ src/csaladfa.service.ts
@@ -1,20 +1,48 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+import { Csaladtag } from './csaladtag.model';
 
 @Injectable()
 export class CsaladfaService {
-  private csaladtagok: Csaladtag[] = [
-    
-  ];
-
-  constructor(
-    private backend: BackendService,
-    private logger: Logger) { }
-
-  getHeroes() {
-    this.backend.getAll(Hero).then( (heroes: Hero[]) => {
-      this.logger.log(`Fetched ${heroes.length} heroes.`);
-      this.heroes.push(...heroes); // fill cache
-    });
-    return this.heroes;
+  getCsaladfa(): Observable<Csaladtag[]> {
+    return of(csaladfa);
   }
 }
+
+const csaladfa: Csaladtag[] = [
+  {
+    id: 1,
+    nev: 'Apa',
+    kor: 40,
+    generacio: 0,
+    apa: 0,
+    anya: 0,
+    par: 2,
+  },
+  {
+    id: 2,
+    nev: 'Anya',
+    kor: 37,
+    generacio: 0,
+    apa: 0,
+    anya: 0,
+    par: 1,
+  },
+  {
+    id: 3,
+    nev: 'Gyerek 1',
+    kor: 12,
+    generacio: 1,
+    apa: 1,
+    anya: 2,
+  },
+  {
+    id: 4,
+    nev: 'Gyerek 2',
+    kor: 7,
+    generacio: 1,
+    apa: 1,
+    anya: 2,
+  },
+];
